6d9cd0c5dbc4fa81acb72f80bb0ba4adfbf609b6
Marcos Vicente Espasandín <tarde@iMac-de-Profesor-3.local>
2015-05-22 19:20:37 +0200

    Initial Commit

---
+++ Ej06/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Ej06
+//
+//  Created by tarde on 22/5/15.
+//  Copyright (c) 2015 a13marcosve. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
